94439bdba7667b24be201364fe2f5799c762c99e
Sidorenko Vladimir <yoyavova@gmail.com>
2009-10-05 15:22:34 +0300

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="django-sso",
+           version="0.1",
+           description="Accept and create single sign on links",
+           author="pyroman",
+           packages=find_packages(),
+           include_package_data=True,
+)
+

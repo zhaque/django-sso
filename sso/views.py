60694fe3a13dcc1d60cd8eb001b61ef55fdf12a9
Arvid Paeglit <arvid@arvid-desktop.(none)>
2009-11-22 17:12:58 +0200

    corrected url construction

--- sso/views.py
+++ sso/views.py
@@ -1,6 +1,6 @@
 import time
+import urllib2
 
-from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.sites.models import Site
 from django.http import HttpResponsePermanentRedirect
@@ -12,16 +12,17 @@ from sso.util import generate_sso_token
 def sso(request):
     next = request.GET.get('next', False)
     if next:
-        id = request.user.id
-        timestamp = time.time()
-        token = generate_sso_token(id, timestamp)
-        url = "%s?id=%s&timestamp=%s&token=%s" % (next, id, timestamp, token)
+        user_id, timestamp = request.user.id, time.time()
+        token = generate_sso_token(user_id, timestamp)
+        
+        path, query = urllib2.splitquery(next)
+        query = "%s&id=%s&timestamp=%s&token=%s&" % (query or '', user_id, timestamp, token)
+        url = "%s?%s" % (path, query)
     else:
         url = request.META.get('HTTP_REFERER', '')
         if not url:
             url = Site.objects.get_current().domain
     if not url.startswith('http://') and not url.startswith('https://'):
         url = 'http://' + url
-
     
     return HttpResponsePermanentRedirect(url)
